2496a3a604138d0862ff87fce0236c0da2bd5c5c
Diana Lipanova <altere2804@gmail.com>
2023-06-14 16:49:55 +0200

    feat: create Register component

--- src/features/auth/register/Register.tsx
+++ src/features/auth/register/Register.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import {useAppDispatch} from "app/hooks";
+import {authThunks} from "features/auth/auth.slice";
+import s from "./Register.module.css"
 
 export const Register = () => {
+    const dispatch = useAppDispatch();
+
+    const registerHandler = () => {
+        const payload = {
+            email: "altere2804@gmail.com",
+            password: "P@ssword"
+        }
+        dispatch(authThunks.register(payload));
+    };
+
     return (
-        <div>
-            REGISTER
+        <div className={s.container}>
+            <h1>Register</h1>
+            <button onClick={registerHandler}>register</button>
         </div>
     );
 };
