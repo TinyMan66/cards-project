e585f008a521b1b673ad067bab72e747b06f582c
Diana Lipanova <altere2804@gmail.com>
2023-06-14 16:50:04 +0200

    feat: create Login component

--- src/features/auth/login/Login.tsx
+++ src/features/auth/login/Login.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import {useAppDispatch} from "app/hooks";
+import {authThunks} from "features/auth/auth.slice";
+import s from "./Login.module.css"
 
 export const Login = () => {
+    const dispatch = useAppDispatch();
+
+    const loginHandler = () => {
+        const payload = {
+            email: 'altere2804@gmail.com',
+            password: 'P@ssw0rd',
+            rememberMe: false
+        }
+        dispatch(authThunks.login(payload));
+    };
+
     return (
-        <div>
-            LOGIN
+        <div className={s.container}>
+            <h1>Login</h1>
+            <button onClick={loginHandler}>login</button>
         </div>
     );
 };
